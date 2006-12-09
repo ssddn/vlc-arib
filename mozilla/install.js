e93dade525f12ea4cfd7deda3254d8758c6c29f3
Derk-Jan Hartman <hartman@videolan.org>
2006-12-09 01:32:42 +0000

    * Update version to 0.8.6 * Update NEWS just a tad * Update contrib in INSTALL.win32

--- mozilla/install.js
+++ mozilla/install.js
@@ -1,4 +1,4 @@
-var version = "0.8.6-rc1";
+var version = "0.8.6";
 
 initInstall( "VideoLAN", "VLC", version, 1);
 
@@ -13,9 +13,9 @@ if ( ! fileExists( tmpFolder) )
 
 setPackageFolder( tmpFolder );
 
-addFile( "http://downloads.videolan.org/pub/videolan/testing/0.8.6-rc1/win32/vlc-0.8.6-rc1-win32.exe" );
+addFile( "http://downloads.videolan.org/pub/videolan/vlc/0.8.6/win32/vlc-0.8.6-win32.exe" );
 
-var exe  = getFolder(tmpFolder, "vlc-0.8.6-rc1-win32.exe");
+var exe  = getFolder(tmpFolder, "vlc-0.8.6-win32.exe");
 File.execute( exe );
 
 performInstall();
