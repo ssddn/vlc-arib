47011de866b91f60e7ba92ebee417fe61484b37d
Rémi Denis-Courmont <rem@videolan.org>
2007-02-20 19:02:32 +0000

    x-plgroup should really be a session-level attribute

--- src/stream_output/sap.c
+++ src/stream_output/sap.c
@@ -643,19 +643,19 @@ static char *SDPGenerate( sap_handler_t *p_sap,
                         "a=recvonly\r\n"
                         "a=type:broadcast\n"
                         "%s"
-                        "m=video %d %s %d\r\n"
-                        "%s%s%s",
+                        "%s%s%s"
+                        "m=video %d %s %d\r\n",
                         i_sdp_id, i_sdp_version,
                         ipv, p_addr->psz_machine,
                         psz_name, ipv, psz_uri,
                         /* FIXME: 1 is IPv4 default TTL, not that of IPv6 */
                         p_session->i_ttl ?: (config_GetInt( p_sap, "ttl" ) ?: 1),
                         (sfilter != NULL) ? sfilter : "",
+                        psz_group ? "a=x-plgroup:" : "",
+                        psz_group ? psz_group : "", psz_group ? "\r\n" : ""
                         p_session->i_port,
                         p_session->b_rtp ? "RTP/AVP" : "udp",
-                        p_session->i_payload,
-                        psz_group ? "a=x-plgroup:" : "",
-                        psz_group ? psz_group : "", psz_group ? "\r\n" : "");
+                        p_session->i_payload);
     if (sfilter != NULL)
         free (sfilter);
 
