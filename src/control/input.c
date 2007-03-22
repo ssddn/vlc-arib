9ba0769a3aed72fb8414c349133931fee0cfe21d
Jean-Paul Saman <jpsaman@videolan.org>
2007-03-22 13:50:34 +0000

    Backport of rev [19383]

--- src/control/input.c
+++ src/control/input.c
@@ -205,7 +205,7 @@ int libvlc_input_get_state( libvlc_input_t *p_input,
 
     p_input_thread = libvlc_get_input_thread ( p_input, p_e);
     if ( libvlc_exception_raised( p_e ) )
-        return 6; /* on error return ERROR_S state (see include/vlc_input.h) */
+        return 0;
 
     var_Get( p_input_thread, "state", &val );
     vlc_object_release( p_input_thread );
