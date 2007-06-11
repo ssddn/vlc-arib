8db744539d664cc8cef010487c4fe4f68600422f
Damien Fouilleul <damienf@videolan.org>
2007-06-11 12:45:39 +0000

    ffmpeg: backport [20343]

--- modules/codec/ffmpeg/video.c
+++ modules/codec/ffmpeg/video.c
@@ -193,7 +193,7 @@ static inline picture_t *ffmpeg_NewPictBuf( decoder_t *p_dec,
         p_dec->fmt_out.video.i_frame_rate =
             p_dec->fmt_in.video.i_frame_rate;
         p_dec->fmt_out.video.i_frame_rate_base =
-            p_dec->fmt_out.video.i_frame_rate_base;
+            p_dec->fmt_in.video.i_frame_rate_base;
     }
     else
 #if LIBAVCODEC_BUILD >= 4754
