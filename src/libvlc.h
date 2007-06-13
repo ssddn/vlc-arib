c834dfb92b57178c0981dd4986a54502e1f2b14f
Christophe Mutricy <xtophe@videolan.org>
2007-06-13 21:34:20 +0000

    Backport the l10n from trunk, activate arabic and persian

--- src/libvlc.h
+++ src/libvlc.h
@@ -29,18 +29,19 @@
 
 #if defined (WIN32) || defined (__APPLE__)
 static char *ppsz_language[] =
-{ "auto", "en", "en_GB", "ca", "cs", "da", "de", "es", "fr", "gl", "he", "hu",
-  "it", "ja", "ka", "ko", "ms", "ne", "nl", "oc", "pt_BR", "ro", "ru", "sk", 
-  "sl",  "sv", "th", "tr", "zh_CN", "zh_TW" };
+{ "auto", "en", "en_GB", "ar", "ca", "cs", "da", "de", "es", "fa", "fr", "gl",
+  "he", "hu", "it", "ja", "ka", "ko", "ms", "ne", "nl", "oc", "pt_BR", "ro",
+  "ru", "sk", "sl",  "sv", "th", "tr", "zh_CN", "zh_TW" };
 
 static char *ppsz_language_text[] =
 { N_("Auto"), N_("American English"), N_("British English"),
-N_("Catalan"), N_("Czech"), N_("Danish"), N_("German"), N_("Spanish"),
-N_("French"), N_("Galician"), N_("Hebrew"), N_("Hungarian"),N_("Italian"),
-N_("Japanese"),N_("Georgian"), N_("Korean"), N_("Malay"), N_("Nepali"), 
-N_("Dutch"), N_("Occitan"), N_("Brazilian Portuguese"), N_("Romanian"), 
-N_("Russian"), N_("Slovak"), N_("Slovenian"), N_("Swedish"), N_("Thai"),
-N_("Turkish"),N_("Simplified Chinese"), N_("Chinese Traditional") };
+N_("Arabic"), N_("Catalan"), N_("Czech"), N_("Danish"), N_("German"), 
+N_("Spanish"), N_("Persian"),N_("French"), N_("Galician"), N_("Hebrew"),
+N_("Hungarian"),N_("Italian"),N_("Japanese"),N_("Georgian"), N_("Korean"),
+N_("Malay"), N_("Nepali"), N_("Dutch"), N_("Occitan"), 
+N_("Brazilian Portuguese"), N_("Romanian"), N_("Russian"), N_("Slovak"), 
+N_("Slovenian"), N_("Swedish"), N_("Thai"), N_("Turkish"),
+N_("Simplified Chinese"), N_("Chinese Traditional") };
 #endif
 
 static char *ppsz_snap_formats[] =
