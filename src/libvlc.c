ssddn/vlc-arib

commit 3033a3df273abe36600523dcb1be98e686b666b5

Fix a-few-per-libvlc-instance memory leaks
Signed-off-by: Jean-Paul Saman <jpsaman@videolan.org>

src/libvlc.c
@@ -945,7 +945,7 @@ int VLC_CleanUp( int i_object )
         stats_HandlerDestroy( p_stats );
         vlc_object_detach( (vlc_object_t*) p_stats );
         vlc_object_release( (vlc_object_t *)p_stats );
-        // TODO: Delete it
+        vlc_object_destroy( p_stats );
     }
 
     /*
@@ -2556,12 +2556,14 @@ static void InitDeviceValues( vlc_t *p_vlc )
     DBusError       error;
 
 #ifdef HAVE_HAL_1
-    ctx =  libhal_ctx_new();
+    ctx = libhal_ctx_new();
     if( !ctx ) return;
     dbus_error_init( &error );
     p_connection = dbus_bus_get ( DBUS_BUS_SYSTEM, &error );
     if( dbus_error_is_set( &error ) )
     {
+        libhal_ctx_shutdown( ctx, &error );
+        libhal_ctx_free( ctx );
         dbus_error_free( &error );
         return;
     }
@@ -2622,6 +2624,7 @@ static void InitDeviceValues( vlc_t *p_vlc )
 
 #ifdef HAVE_HAL_1
         libhal_ctx_shutdown( ctx, NULL );
+        libhal_ctx_free( ctx );
 #else
         hal_shutdown( ctx );
 #endif

commit fde40cced2587b9049da1c8d79657813b78eca0a

Fix #1589 - do not shutdown an uninitialized hal context
Regression was introduced in [3033a3df273abe36600523dcb1be98e686b666b5]

src/libvlc.c
@@ -2562,7 +2562,6 @@ static void InitDeviceValues( vlc_t *p_vlc )
     p_connection = dbus_bus_get ( DBUS_BUS_SYSTEM, &error );
     if( dbus_error_is_set( &error ) )
     {
-        libhal_ctx_shutdown( ctx, &error );
         libhal_ctx_free( ctx );
         dbus_error_free( &error );
         return;