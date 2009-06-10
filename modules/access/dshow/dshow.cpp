a6110fb1d253146810bbe5f9b5ba0097b72b4e3c
Jean-Baptiste Kempf <jb@videolan.org>
2009-06-10 03:04:09 +0200

    DShow: fix configure dialog probe

--- modules/access/dshow/dshow.cpp
+++ modules/access/dshow/dshow.cpp
@@ -2008,20 +2008,25 @@ static int ConfigDevicesCallback( vlc_object_t *p_this, char const *psz_name,
 {
     module_config_t *p_item;
     bool b_audio = false;
+    const char *psz_device = NULL;
+
+    if( !EMPTY_STR( newval.psz_string ) )
+        psz_device = strdup( newval.psz_string );
 
     /* Initialize OLE/COM */
     CoInitialize( 0 );
 
     p_item = config_FindConfig( p_this, psz_name );
+
     if( !p_item ) return VLC_SUCCESS;
 
     if( !strcmp( psz_name, "dshow-adev" ) ) b_audio = true;
 
     string devicename;
 
-    if( newval.psz_string && *newval.psz_string )
+    if( psz_device )
     {
-        devicename = newval.psz_string;
+        devicename = psz_device ;
     }
     else
     {
