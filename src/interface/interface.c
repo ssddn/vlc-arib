66fab58c1348e6d2dd8ffe8ca9f224c4b2dfc6ba
Erwan Tulou <erwan10@videolan.org>
2009-06-23 18:52:37 +0200

    MacOs and Windows(skins): don't kill and join twice the same thread

--- src/interface/interface.c
+++ src/interface/interface.c
@@ -140,11 +140,8 @@ int intf_RunThread( intf_thread_t *p_intf )
         }
         RunInterface( VLC_OBJECT(p_intf) );
 
-        /* Make sure our MonitorLibVLCDeath thread exit */
-        vlc_object_kill( p_intf );
         /* It is monitoring libvlc, not the p_intf */
         vlc_object_kill( p_intf->p_libvlc );
-        vlc_thread_join( p_intf );
 
         return VLC_SUCCESS;
     }
