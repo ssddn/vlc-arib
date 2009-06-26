5e07008c220ef2675280aeba6b3710363e37ae1a
Derk-Jan Hartman <hartman@videolan.org>
2009-06-26 17:08:45 +0200

    macosx: Don't try to guess a name anymore. This is now done in the core. (cherry picked from commit 28fd4de956987ccf65cfe02f25363839c9b5af1a)
    Signed-off-by: Derk-Jan Hartman <hartman@videolan.org>

--- modules/gui/macosx/playlist.m
+++ modules/gui/macosx/playlist.m
@@ -1064,8 +1064,6 @@
             }
         }
     }
-    /* If no name, then make a guess */
-    if( !o_name) o_name = [[NSFileManager defaultManager] displayNameAtPath: o_uri];
 
     if( [[NSFileManager defaultManager] fileExistsAtPath:o_uri isDirectory:&b_dir] && b_dir &&
         [[NSWorkspace sharedWorkspace] getFileSystemInfoForPath: o_uri isRemovable: &b_rem
@@ -1087,7 +1085,7 @@
         o_uri = o_temp;
     }
 
-    p_input = input_item_New( p_playlist, [o_uri fileSystemRepresentation], [o_name UTF8String] );
+    p_input = input_item_New( p_playlist, [o_uri fileSystemRepresentation], o_name ? [o_name UTF8String] : NULL );
     if( !p_input )
     {
         pl_Release( p_intf );
